4acef8a38cbd1428bfca6c6a0f6bcbff73e5700c
justin-h-im <imjustin679@gmail.com>
2025-01-25 19:04:33 +0000

    initial neck motor control

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 import time
 import smbus2
 from adafruit_servokit import ServoKit
+from motorFunction import motorInit
+from motorFunction import turnRight
+from motorFunction import stopMotor
 
-
+bus = smbus2.SMBus(1)
 
 def run():
-    ;lkj
+    motorInit()
+    for i in range(1):
+        turnRight()
+    stopMotor()
+    print("Motor Running")
 
-if __name__ == '__main':
+if __name__ == '__main__':
+    print("Running main")
     run()
