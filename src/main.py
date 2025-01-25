fdd8020fb1273aae0d97cec43861e488624c4882
justin-h-im <imjustin679@gmail.com>
2025-01-25 19:48:31 +0000

    broadcast 1920x1080

--- src/main.py
+++ src/main.py
@@ -5,10 +5,13 @@ from motorFunction import motorInit
 from motorFunction import turnRight
 from motorFunction import turnLeft
 from motorFunction import stopMotor
+from cameraFeed import cameraInit
 
 bus = smbus2.SMBus(1)
 
 def run():
+    cameraInit()
+
     motorInit()
     for i in range(1):
         turnLeft()
