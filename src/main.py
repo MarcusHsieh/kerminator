d66e12ea4e4b847f4d15693bc39d4e7ceb94fac1
justin-h-im <imjustin679@gmail.com>
2025-01-25 19:09:21 +0000

    implemented left and right correctly

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import smbus2
 from adafruit_servokit import ServoKit
 from motorFunction import motorInit
 from motorFunction import turnRight
+from motorFunction import turnLeft
 from motorFunction import stopMotor
 
 bus = smbus2.SMBus(1)
@@ -10,7 +11,10 @@ bus = smbus2.SMBus(1)
 def run():
     motorInit()
     for i in range(1):
+        turnLeft()
+        time.sleep(3)
         turnRight()
+        time.sleep(3)
     stopMotor()
     print("Motor Running")
 
