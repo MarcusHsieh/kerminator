0253a6655c8aea2aa8ce5e9e4002e45b078d8768
MarcusHsieh <marcus.j.hsieh@gmail.com>
2025-01-26 02:58:19 -0800

    rm

--- mouth/mouth_movement.py
+++
@@ -1,9 +0,0 @@
-# implement function that takes an array of time offsets and RMS values, and moves servos accordingly
-
-import time
-import board
-import busio
-import adafruit_pca9685
-import json
-
-# WIP
