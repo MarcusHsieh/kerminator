8a450a88dddc14bd266cb79f888facfbd44e8d2d
Marcus Hsieh <80995617+MarcusHsieh@users.noreply.github.com>
2025-01-25 19:19:45 -0800

    Laptop and mouth directories \\ Implemented `file_watcher.py` \\ WIP `\Laptop` and `mouth_movement.py`

---
+++ mouth/mouth_movement.py
@@ -0,0 +1,9 @@
+# implement function that takes an array of time offsets and RMS values, and moves servos accordingly
+
+import time
+import board
+import busio
+import adafruit_pca9685
+import json
+
+# WIP
